07554f9c5b77f85d83a2262884e540c6f956fbea
Amirul Islam <35567854+shiningflash@users.noreply.github.com>
2019-07-16 15:27:57 +0600

    Update sqrt_decomposition.cpp

--- sqrt_decomposition.cpp
+++ sqrt_decomposition.cpp
@@ -1,32 +1,56 @@
+// sqrt decomposition
+// @author: Amirul Islam
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // freopen("in", "r", stdin);
-    int n, l, r, q;
-    scanf("%d", &n);
+void getsum(int a[], int n, int b[], int m, int l, int r) {
+    int sum = 0, lb = l/m, rb = r/m;
+    if (lb == rb) for (int i = l; i <= r; sum += a[i++]);
+    else {
+        for (int i = l, end = (lb+1)*m-1; i <= end; sum += a[i++]);
+        for (int i = lb+1; i < rb; sum += b[i++]);
+        for (int i = rb*m; i <= r; sum += a[i++]);
+    }
+    //for (int i = 0; i < m; i++) cout << b[i] << " "; cout << endl;
+    printf("%d\n", sum);
+}
 
-    // main array
-    int a[n];
-    for (int i = 0; i < n; scanf("%d", &a[i++]));
-
-    // decomposed array, processing
-    int len = (int) sqrt(n);
-    len += !(len*len==n); // each block length and total block number
-    int b[len];
+void sqrt_decomposition(int a[], int n) {
+    int m = sqrt(n), q, l, r;
+    m += !(m*m == n);
+    int b[m];
     memset(b, 0, sizeof(b));
-    for (int i = 0; i < n; b[i/len] += a[i++]);
+    for (int i = 0; i < n; i++) b[i/m] += a[i];
 
-    // answering the queries
     for (scanf("%d", &q); q--; ) {
         scanf("%d %d", &l, &r);
-        int sum = 0, left_block = l/len, right_block = r/len;
-        if (left_block == right_block) for (int i = l; i <= r; sum += a[i++]); // range is in same block
-        else {
-            for (int i = l, end = (left_block+1)*len-1; i <= end; sum += a[i++]); // left most block sum
-            for (int i = left_block+1; i < right_block; sum += b[i++]); // middle all block's sum
-            for (int i = right_block*len; i <= r; sum += a[i++]); // right most block sum
-        }
-        printf("%d\n", sum);
+        getsum(a, n, b, m, l, r);
     }
 }
+
+int main() {
+    //freopen("in", "r", stdin);
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    sqrt_decomposition(a, n);
+}
+
+/*
+---------
+INPUT:
+9
+1 1 1 2 2 2 3 3 3
+3
+1 3
+8 8
+0 8
+---------
+OUTPUT:
+4
+3
+18
+---------
+*/
