787fff90c1d815639c730aa39d2be91084ee462e
Amirul Islam <amirulislamalmamun@gmail.com>
2021-01-27 19:54:00 +0600

    added priority queue in csharp

--- CSHARP/Program.cs
+++ CSHARP/Program.cs
@@ -5,7 +5,16 @@ public class Program {
     public static void Main(string[] args) {
         string[] lines = System.IO.File.ReadAllLines("in.txt");
 
-        MapExercise me = new MapExercise();
-        me.run(lines);
+        // MapExercise me = new MapExercise();
+        // me.run(lines);
+
+        PriorityQueue<int> pq = new PriorityQueue<int>();
+        pq.Add(12, 12);
+        pq.Add(13, 13);
+        pq.Add(2, 2);
+        Console.WriteLine(pq.RemoveMin());
+        Console.WriteLine(pq.Peek());
+        Console.WriteLine(pq.RemoveMin());
+        Console.WriteLine(pq.RemoveMin());
     }
 }
