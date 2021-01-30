3b60daae2ddbf6f7a658e6f1c0b65a8cbf20e1ac
Amirul Islam <amirulislamalmamun@gmail.com>
2021-01-30 09:30:10 +0600

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,80 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define SIZE 10
-
+ 
 class mystack {
-    int *arr;
-    int top;
-    int capacity;
+    
+    public:
+        int *arr;
+        int top;
+        int capacity;
 
-public:
-    mystack(int size = SIZE);
-    void push(int);
-    int pop();
-    int peek();
-    int size();
-    bool isEmpty();
-    bool isFull();
+    mystack(int size) {
+        arr = new int[size];
+        capacity = size;
+        top = -1;
+    }
+    
+    void push(int item) {
+        if (isFull()) {
+            printf("Error: stack overflow.");
+            return;
+        }
+        arr[++top] = item;
+    }
+    
+    void pop() {
+        if (isEmpty()) {
+            printf("Error: stack is empty.");
+            return;
+        }
+        top--;
+    }
+    
+    int peek() {
+        if (isEmpty()) {
+            printf("Error: stack is empty.");
+            exit(EXIT_FAILURE);
+        }
+        return arr[top];
+    }
+    
+    int size() {
+        return top + 1;
+    }
+    
+    bool isEmpty() {
+        return top == -1;
+    }
+    
+    bool isFull() {
+        return top == capacity - 1;
+    }
+    
+    void print_stack(mystack st) {
+        while (!st.isEmpty()) {
+            printf("%d ", st.peek());
+            st.pop();
+        }
+        printf("\n");
+    }
 };
 
-mystack::mystack(int size) {
-    arr = new int[size];
-    capacity = size;
-    top = -1;
-}
-
-void mystack::push(int item) {
-    if (isFull()) {
-        printf("Eroor: stack overflow\n");
-        return;
-    }
-    arr[++top] = item;
-}
-
-int mystack::pop() {
-    if (isEmpty()) {
-        printf("Error: stack is empty\n");
-        exit(EXIT_FAILURE);
-    }
-    return arr[top--];
-}
-
-int mystack::peek() {
-    if (isEmpty()) {
-        printf("Error: stack is empty\n");
-        exit(EXIT_FAILURE);
-    }
-    return arr[top];
-}
-
-int mystack::size() {
-    return top + 1;
-}
-
-bool mystack::isEmpty() {
-    return top == -1;
-}
-
-bool mystack::isFull() {
-    return top == capacity - 1;
-}
-
-void print_stack(mystack st) {
-    while (!st.isEmpty()) {
-        printf("%d ", st.pop());
-    }
-    printf("\n");
-}
-
 int main() {
-    mystack st(5);
+    mystack st(10);
     st.push(1);
     st.push(2);
     st.push(3);
-    st.push(4);
-    print_stack(st); // 4 3 2 1
-    printf("%d\n", st.pop()); // 4
-    printf("%d\n", st.peek()); // 3
-    print_stack(st); // 3 2 1
-    return 0;
+    st.print_stack(st);
+    st.pop();
+    st.pop();
+    st.print_stack(st);
 }
